976ee9b93d8747a32fe22f1e36f1d5313c6df275
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-22 19:52:23 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// In questo esercizio, dovrai creare una classe in JavaScript chiamata "Veicolo" con alcune proprietà e metodi.
+// La classe "Veicolo" dovrebbe avere le seguenti caratteristiche:
+// marca
+// anno
+// colore
+// porte
+// carburante
+
+
+
+
+
+   // Creiamo una nuova istanza di veicolo che rappresenti una Fiat 2019 blu.
+    
+    // Otteniamo le informazioni della vettura utilizzando il metodo informazioni()
+
+     // Calcoliamo l'età della vettura utilizzando il metodo calcolaEta()
+
+    //  Stampiamo i risultati in console 
